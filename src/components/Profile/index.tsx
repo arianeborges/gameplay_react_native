70e7665605425c0223f2fceea29c177fd815e335
Ariane <ariianeboorges@gmail.com>
2021-07-01 16:11:05 +0100

    Remove unwanted imports

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { View, Text } from 'react-native';
 import { Avatar } from '../Avatar';
 
 import { styles } from './styles';
