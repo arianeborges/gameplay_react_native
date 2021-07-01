6fd6aa3df2d36cf20ff5308c49cf1a6427e46590
Ariane <ariianeboorges@gmail.com>
2021-07-01 14:10:51 +0100

    Add Home page screen

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
+
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -23,7 +25,7 @@ export default function App() {
   return (
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <SignIn />
+      <Home />
     </Background>
   );
 }
