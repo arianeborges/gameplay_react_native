272ece873276747c41826c1d15023a70b8946cfb
Ariane <ariianeboorges@gmail.com>
2021-07-02 11:17:56 +0100

    Fix the checked view

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -20,10 +20,11 @@ export function Category({ title, icon: Icon, checked = false, ...rest }: Props)
       <RectButton {...rest}>
          <LinearGradient style={styles.container} colors={[secondary50, secondary70]}>
             <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-               <View style={checked ? styles.checked : styles.check}>
-                  <Icon width={48} height={48} />
-                  <Text>{title}</Text>
-               </View>
+               <View style={checked ? styles.checked : styles.check} />
+
+               <Icon width={48} height={48} />
+               <Text style={styles.title}>{title}</Text>
+               
             </View>
          </LinearGradient>
       </RectButton>
