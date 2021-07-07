01caf80e827279bbd3593c47216dc6de1bbdac3c
Ariane <ariianeboorges@gmail.com>
2021-07-07 14:38:09 +0100

    Add Modal View to Create Appointment (not working)

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,96 +1,116 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, ScrollView, Text, View } from 'react-native';
+import { KeyboardAvoidingView, ScrollView, Text, View, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
-import { Background } from '../../components/Background';
 import { CategorySelect } from '../../components/CategorySelect';
 import { Header } from '../../components/Header';
 import { GuildIcon } from '../../components/GuildIcon';
 import { SmallInput } from '../../components/SmallInput';
 import { TextArea } from '../../components/TextArea';
+import { Button } from '../../components/Button';
+import { GuildProps } from '../../components/Appointment';
+import { ModalView } from '../../components/ModalView';
+import { Guilds } from '../Guilds';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
-import { Platform } from 'react-native';
-import { Button } from '../../components/Button';
-
+import { Background } from '../../components/Background';
 
 export function AppointmentCreate() {
    const [category, setCategory] = useState('');
+   const [openGuildsModal, setOpenGuildsModal] = useState(false);
+   const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+   function handleOpenGuilds() {
+      setOpenGuildsModal(true);
+   }
+
+   function handleGuildSelected(guildSelected: GuildProps) {
+      setGuild(guildSelected);
+      setOpenGuildsModal(false);
+   }
 
    return (
       <KeyboardAvoidingView
          style={styles.container}
          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
-         <ScrollView>
-            <Background>
-            <Header title="Schedule match" />
+         <Background>
+            <ScrollView>
+               <Header title="Schedule match" />
 
-            <Text style={[styles.label, { marginLeft: 24, marginTop: 36, marginBottom: 18 }]}>Category</Text>
+               <Text style={[styles.label, { marginLeft: 24, marginTop: 36, marginBottom: 18 }]}>Category</Text>
 
-            <CategorySelect
-               hasCheckBox
-               setCategory={setCategory}
-               categorySelected={category}
-            />
-
-            <View style={styles.form}>
-               <RectButton>
-                  <View style={styles.select}>
-                     {/* <View style={styles.image} /> */}
-                     <GuildIcon />
-
-                     <View style={styles.selectBody}>
-                        <Text style={styles.label}>Select a Server</Text>
-                     </View>
-
-                     <Feather
-                        name="chevron-right"
-                        color={theme.colors.heading}
-                        size={18}
-                     />
-                  </View>
-               </RectButton>
-
-               <View style={styles.field}>
-                  <View>
-                     <Text style={styles.label}>Day and month</Text>
-                     <View style={styles.column}>
-                        <SmallInput maxLength={2} />
-                        <Text style={styles.divider}>/</Text>
-                        <SmallInput maxLength={2} />
-                     </View>
-                  </View>
-
-                  <View>
-                     <Text style={styles.label}>Hour and minute</Text>
-                     <View style={styles.column}>
-                        <SmallInput maxLength={2} />
-                        <Text style={styles.divider}>:</Text>
-                        <SmallInput maxLength={2} />
-                     </View>
-                  </View>
-               </View>
-
-               <View style={[styles.field, { marginBottom: 12 }]}>
-                  <Text style={styles.label}>Description</Text>
-                  <Text style={styles.charactersLimit}>Max 100 characters</Text>
-               </View>
-
-               <TextArea
-                  multiline
-                  maxLength={100}
-                  numberOfLines={5}
-                  autoCorrect={false}
+               <CategorySelect
+                  hasCheckBox
+                  setCategory={setCategory}
+                  categorySelected={category}
                />
 
-               <View style={styles.footer}>
-                  <Button title="Schedule" />
+               <View style={styles.form}>
+                  <RectButton onPress={handleOpenGuilds}>
+                     <View style={styles.select}>
+                        {
+                           guild.icon
+                              ? <GuildIcon />
+                              : <View style={styles.image} />
+                        }
+                        <View style={styles.selectBody}>
+                           <Text style={styles.label}>
+                              {guild.name ? guild.name : 'Select a Server'}
+                           </Text>
+                        </View>
+
+                        <Feather
+                           name="chevron-right"
+                           color={theme.colors.heading}
+                           size={18}
+                        />
+                     </View>
+                  </RectButton>
+
+                  <View style={styles.field}>
+                     <View>
+                        <Text style={styles.label}>Day and month</Text>
+                        <View style={styles.column}>
+                           <SmallInput maxLength={2} />
+                           <Text style={styles.divider}>/</Text>
+                           <SmallInput maxLength={2} />
+                        </View>
+                     </View>
+
+                     <View>
+                        <Text style={styles.label}>Hour and minute</Text>
+                        <View style={styles.column}>
+                           <SmallInput maxLength={2} />
+                           <Text style={styles.divider}>:</Text>
+                           <SmallInput maxLength={2} />
+                        </View>
+                     </View>
+                  </View>
+
+                  <View style={[styles.field, { marginBottom: 12 }]}>
+                     <Text style={styles.label}>Description</Text>
+                     <Text style={styles.charactersLimit}>Max 100 characters</Text>
+                  </View>
+
+                  <TextArea
+                     multiline
+                     maxLength={100}
+                     numberOfLines={5}
+                     autoCorrect={false}
+                  />
+
+                  <View style={styles.footer}>
+                     <Button title="Schedule" />
+                  </View>
                </View>
-            </View>
-         </Background >
-         </ScrollView>
+            </ScrollView>
+         </Background>
+
+         <ModalView visible={openGuildsModal}>
+            <Guilds handleGuildSelected={handleGuildSelected} />
+         </ModalView>
       </KeyboardAvoidingView>
    )
 }
