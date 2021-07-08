f5162c3e19364585bd0cc08575a0a44da3004f03
Ariane <ariianeboorges@gmail.com>
2021-07-08 18:08:59 +0100

    Add authentication with Discord

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { LogBox, StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
 
 import { Background } from './src/components/Background';
 import { Routes } from './src/routes';
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,18 @@
 import React, { createContext, ReactNode, useContext, useState } from 'react';
 
+import * as AuthSession from 'expo-auth-session';
+
+import {
+   REDIRECT_URI,
+   SCOPE,
+   RESPONSE_TYPE,
+   CLIENT_ID,
+   CDN_IMAGE
+} from '../configs';
+
+import { api } from '../services/api';
+
+
 type User = {
    id: string;
    username: string;
@@ -11,6 +24,7 @@ type User = {
 
 type AuthContextData = {
    user: User;
+   signIn: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -21,9 +35,24 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
    const [user, setUser] = useState<User>({} as User);
+   const [loading, setLoading] = useState(false);
+
+   async function signIn() {
+      try {
+         setLoading(true);
+
+         const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
+
+         const response = await AuthSession.startAsync({ authUrl });
+         console.log(response);
+
+      } catch (error) {
+         throw new Error('It was not possible to authenticate');
+      }
+   }
 
    return (
-      <AuthContext.Provider value={{ user }}>
+      <AuthContext.Provider value={{ user, signIn }}>
          {children}
       </AuthContext.Provider>
    )
