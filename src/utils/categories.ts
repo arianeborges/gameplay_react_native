1ba7f255a447d88f02d168779a752700fc1cd585
Ariane <ariianeboorges@gmail.com>
2021-07-14 13:13:32 +0100

    Refact translate categories

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -4,8 +4,8 @@ import FunSvg from '../assets/fun.svg';
 import TrainingSvg from '../assets/training.svg';
 
 export const categories = [
-  { id: '1', title: 'Ranqueada', icon: RankedSvg },
-  { id: '2', title: 'Duelo 1x1', icon: DuelSvg },
-  { id: '3', title: 'Diversão', icon: FunSvg },
-  { id: '4', title: 'Treino', icon: TrainingSvg },
+  { id: '1', title: 'Ranked', icon: RankedSvg },
+  { id: '2', title: 'Duel 1x1', icon: DuelSvg },
+  { id: '3', title: 'Fun', icon: FunSvg },
+  { id: '4', title: 'Training', icon: TrainingSvg },
 ]
