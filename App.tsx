c4abe03151b0b73157f79633b13546091ef6f097
Ariane <ariianeboorges@gmail.com>
2021-07-22 11:28:33 +0100

    Add Async Storage clear function to reset all saved data

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
 
@@ -22,6 +23,8 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />
   }
+
+  //AsyncStorage.clear();
 
   return (
     <Background>
